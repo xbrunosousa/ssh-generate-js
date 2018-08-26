a3218ebb6d4a1845c5ae29417ede139be5365e83
Bruno Vinícius <mail@bruno.vin>
2018-08-26 13:10:24 -0300

    Updates

---
+++ precache-manifest.e504be02a2eca380ad3a9abc86cc79c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "12011c46acaf486324d4",
+    "url": "js/chunk-vendors.a72edac2.js"
+  },
+  {
+    "revision": "26797000e2fac1390b21",
+    "url": "js/app.932d5ef7.js"
+  },
+  {
+    "revision": "c068b2b7978e27d7c51eeb3b0e93892a",
+    "url": "index.html"
+  },
+  {
+    "revision": "26797000e2fac1390b21",
+    "url": "css/app.2f9f37a2.css"
+  }
+];
